eaf9cb592145af55107f4396893d0137189a0c3b
Runar Fredagsvik <runarsf@protonmail.com>
2019-10-10 14:27:06 +0000

    License change and improved package installation

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # deploy
 A bash script made for deploying [dotfiles](https://github.com/runarsf/dotfiles) from a folder or git repo.
 
+### Installation
+
 1. `cd /path/to/dotfiles`
 2. `git clone git@github.com:runarsf/deploy.git` or `git clone https://github.com/runarsf/deploy`
 3. `cd deploy`
 4. `deploy.sh`
 
+### Usage
+```bash
+./deploy.sh --dotfiles /path/to/dotfiles --packages /path/to/packages.json
+```
+
 > **deploy** © [runarsf](https://github.com/runarsf) · Author and maintainer.<br>
-> Released under the [GPL-3.0](https://opensource.org/licenses/GPL-3.0) [License](https://github.com/runarsf/sensugo-discord-handler/blob/master/LICENSE).
+> Released under the [OSL-3.0](https://opensource.org/licenses/OSL-3.0) [License](https://github.com/runarsf/deploy/blob/master/LICENSE).
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
-
-# DEBUG
-#set -x # show all commands being executed
-#trap 'printf "%3d: " "$LINENO"' DEBUG # show line numbers
-#trap 'echo "# $BASH_COMMAND"' DEBUG # best looking debug
-#set -o xtrace
-#(set -x; command) # create a subshell that will debug only one command
+# saner programming env: these switches turn some bugs into errors
+#set -o errexit -o pipefail -o noclobber -o nounset
+set -o errexit -o pipefail -o noclobber
 
 if [ $EUID != 0 ]; then
-  sudo "$0" "-${arg}"
-  exit $?
+    sudo "$0" "$@"
+    # sudo "$0" "-${arg}"
+    exit $?
 fi
 
 prompt() {
@@ -23,66 +20,98 @@ prompt() {
 }
 
 sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dir="$(dirname "${sdir}")"
-prompt "Detected dotfile directory ${dir}. Do you want to change it?" && read -p 'New directory: ' dir && dir=${dir%/}
-if ! [[ -d "${dir}" ]]; then
-  echo 'Not a directory.'
-  exit 1
-fi
 
-include() {
-  echo "Loading ${sdir}/lib/${1}"
-  source ${sdir}/lib/${1}
-}
-include colours.sh
+# Libraries
+source ${sdir}/lib/colours.sh
 
 configs() {
+  # FIXME: File permissions can get set to root only
   now="$(date '+%d%m%y-%H%M%S')"
-  (set -x; mkdir $sdir/backup/$now)
-  cd ${dir}
+  #(set -x; mkdir ${sdir}/backup/${now})
+  cd ${dotfiles}
   for f in * .*; do
-    if ! [[ "$f" =~ ^(\.|\.\.|\.git|\.gitignore|README\.md|deploy|packages\.csv)$ ]]; then
+    if ! [[ "$f" =~ ^(\.|\.\.|\.git|\.gitignore|README\.md|deploy|deploy\.json)$ ]]; then
       if [[ "$f" == "root" ]]; then # could also run for-loop on ./root folder to get all files instead of /*
-        (set -x; cp -rsvu --backup=simple --suffix=".${now}" ${dir}/${f}/* /)
+        (set -x; cp -rsvu --backup=simple --suffix=".${now}" ${dotfiles}/${f}/* /)
       elif [[ -d "$f" ]]; then
-        (set -x; cp -rsvu --backup=simple --suffix=".${now}" ${dir}/${f} ${HOME}/)
+        (set -x; cp -rsvu --backup=simple --suffix=".${now}" ${dotfiles}/${f} ${HOME}/)
       else
-        (set -x; cp -svu --backup=simple --suffix=".${now}" ${dir}/${f} ${HOME}/${f})
+        (set -x; cp -svu --backup=simple --suffix=".${now}" ${dotfiles}/${f} ${HOME}/${f})
       fi
     fi
   done
 }
 
 install() {
-  pkg="${dir}/packages.json"
-  if [[ -f "${pkg}" ]]; then
-    prompt "Detected package database ${pkg}. Do you want to change it?" && read -p 'New package database: ' pkg && pkg=${pkg%/}
-  else
-    prompt "Could not detect package database, please enter a valid location."; read -p 'New package database: ' pkg && pkg=${pkg%/}
-  fi
-  if ! [[ -f "${pkg}" ]]; then
-    echo 'Not a valid file.'
-    exit 1
-  fi
-
   command -v jq >/dev/null 2>&1 \
-  && jq='jq' \
-  || jq="${sdir}/backup/jq" \
-  && [ ! -f ${sdir}/backup/jq ] \
-  && curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output ${sdir}/backup/jq \
-  && chmod +x ${sdir}/backup/jq
+    && jq='jq' \
+    || jq="${sdir}/backup/jq" \
+    && [ ! -f ${sdir}/backup/jq ] \
+    && curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output ${sdir}/backup/jq \
+    && chmod +x ${sdir}/backup/jq
 
-  packages=$(${jq} -r ".packages | .[] | .package" ${pkg})
-  prefix=$(${jq} -r ".prefix" ${pkg})
+  packages=$(${jq} -r ".packages | .[] | .package" ${config})
+  prefix=$(${jq} -r ".prefix" ${config})
   [ -z "$prefix" ] && unset prefix
-  suffix=$(${jq} -r ".suffix" ${pkg})
+  suffix=$(${jq} -r ".suffix" ${config})
   [ -z "$suffix" ] && unset suffix
   while read -r package; do
-    #(set -x; apt install -y ${package})
     (set -x; ${prefix}${package}${suffix})
   done <<< ${packages}
-
-  #(set -x; ${prefix}${package}$(${suffix}))
 }
 
-install
+# Args:
+#  -d:, --dotfiles: - dotfiles directory absolute path
+#  -p:, --packages: - deploy.json file absolute path
+#  -n, --no-backup - disable backup
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -d|--dotfiles)
+      # TODO: Allow relative path name by converting relative to full
+      dotfiles=${2%/}
+      if ! [[ -d "${dotfiles}" ]]; then
+        echo "${dotfiles}: No such directory."
+        exit 1
+      fi
+      configs
+      # dir="$(dirname "${sdir}")" # get parent directory
+      # dir=${dir%/} # remove trailing slash
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--packages)
+      config="$2"
+      if [[ -d "${dotfiles}" ]] && ! [[ "${config}" == *\/* ]] && [ -f "${dotfiles}/${config}" ]; then
+        config="${dotfiles}/${config}"
+      elif ! [[ -f "${config}" ]]; then
+        echo "${config}: No such file."
+        exit 1
+      fi
+      install
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--echo)
+      [ ! -z "$2" ] && echo "$2" \
+        || echo "No value"
+      shift
+      shift
+      ;;
+    -n|--no-backup)
+      backup=n
+      shift # past argument
+      ;;
+    *) # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+    echo "Untracked argument:"
+    echo "$1"
+fi
